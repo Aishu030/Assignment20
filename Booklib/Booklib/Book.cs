cefcbba832e47fce9a79d2f16211a70ac6458273
Aishu030 <aishuramesh031@gmail.com>
2023-08-01 15:40:44 +0530

    first commit

---
+++ Booklib/Booklib/Book.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Booklib
+{
+    public class Book
+    { 
+        public int BookId { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Genre { get; set; }
+        public bool IsAvailable { get; set; }
+    }
+}
---
+++ Booklib/Booklib/Library.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Booklib
+{
+    public class Library
+    {
+        static List<Book> books;
+
+        public Library()
+        {
+            books = new List<Book>();
+        }
+
+        public void AddBook(Book bk)
+        {
+            books.Add(bk);
+        }
+
+        public void ViewAllBooks()
+        {
+            foreach (var bk in books)
+            {
+                Console.WriteLine($"Book ID: {bk.BookId},\nTitle: {bk.Title},\nAuthor: {bk.Author},\nGenre: {bk.Genre},\nAvailable: {bk.IsAvailable}");
+            }
+        }
+        public void SearchBookById(int bookId)
+        {
+            var bk = books.Find(b => b.BookId == bookId);
+            if (bk != null)
+            {
+                Console.WriteLine($"Book ID: {bk.BookId},\nTitle: {bk.Title},\nAuthor: {bk.Author},\nGenre: {bk.Genre},\nAvailable: {bk.IsAvailable}");
+            }
+            else
+            {
+                Console.WriteLine("Book not found!!!!!!!!");
+            }
+        }
+
+        public void SearchBookByTitle(string title)
+        {
+            var bk = books.Find(b => b.Title.Equals(title, StringComparison.OrdinalIgnoreCase));
+            if (bk != null)
+            {
+                Console.WriteLine($"Book ID: {bk.BookId},\nTitle:  {bk.Title} ,\nAuthor:  {bk.Author} ,\nGenre:  {bk.Genre} ,\nAvailable: {bk.IsAvailable}");
+            }
+            else
+            {
+                Console.WriteLine("Book not found!!!!!!!!");
+            }
+        }
+    }
+}
